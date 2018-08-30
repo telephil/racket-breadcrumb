699e8e575fe07a424c261a849bb795fde20fa53b
Philippe Mechai <philippe.mechai@gmail.com>
2018-08-30 20:22:18 +0200

    add gui-lib to deps

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "breadcrumb")
-(define deps '("gui"))
+(define deps '("base" "gui-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/breadcrumb.scrbl" ())))
 (define pkg-desc "A simple breadcrumb widget")
 (define version "0.0")
-(define pkg-authors '("Philippe MECHAI"))
+(define pkg-authors '(pmechai))
